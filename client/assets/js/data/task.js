93b20bfbfaa1278c3b878652ad562e766693ed4f
Cauan <cauanfailla@gmail.com>
2025-09-20 16:25:58 -0300

    refactor(tasks): refatorar funcionalidade

--- client/assets/js/data/task.js
+++ client/assets/js/data/task.js
@@ -12,22 +12,27 @@ export async function getTasks() {
 }
 
 export async function createTaskInServer(form, e) {
-    document.querySelector
-    e.preventDefault()
-    const date = new Date()
-    const formattedDate = date.toISOString().split("T")[0];  
-    const formData = new FormData(form)
-    const tasks = [Object.fromEntries(formData)]
-    tasks[0].date = formattedDate
-    const response = await fetch("http://127.0.0.1:3000/tasks", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(tasks[0])
-    }) 
-    const taskId = await response.json()
-    tasks[0].id = taskId
-    document.querySelector('#ititle').value = ''
-    return tasks
+    try {
+        document.querySelector
+        e.preventDefault()
+        const date = new Date()
+        const formattedDate = date.toISOString().split("T")[0];  
+        const formData = new FormData(form)
+        const tasks = [Object.fromEntries(formData)]
+        tasks[0].date = formattedDate
+        const response = await fetch("http://127.0.0.1:3000/tasks", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(tasks[0])
+        }) 
+        const taskId = await response.json()
+        tasks[0].id = taskId
+        document.querySelector('#ititle').value = ''
+        return tasks
+    } catch (err) {
+        console.error("Erro ao adicionar tarefa", err.message)
+    }
+    
 }
 
 export async function deleteTaskInServer(taskId) {
